84b5c314451acccb25e904e254c5136a23d1349a
zyc <zyclyy@sina.cn>
2014-07-14 18:42:16 +0800

    7-14 BUG_VERSION

---
+++ OutlookMeetingAdd/OutlookMeetingAdd/wificollection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NativeWifi;
+
+namespace OutlookMeetingAdd
+{
+    class WIFISSID
+    {
+        public string SSID = "NONE";
+        public string dot11DefaultAuthAlgorithm = "";
+        public string dot11DefaultCipherAlgorithm = "";
+        public bool networkConnectable = true;
+        public string wlanNotConnectableReason = "";
+        public int wlanSignalQuality = 0;
+        public WlanClient.WlanInterface wlanInterface = null;
+    }  
+
+
+    class wificollection
+    {
+        
+        public List<WIFISSID> ssids = new List<WIFISSID>();
+  
+        public wificollection()
+        {
+            ssids.Clear();
+        }
+  
+  
+        static string GetStringForSSID(Wlan.Dot11Ssid ssid)
+        {
+            return Encoding.UTF8.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+        }
+  
+        public void ScanSSID()
+        {
+            WlanClient client = new WlanClient();
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+                Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+                foreach (Wlan.WlanAvailableNetwork network in networks)
+                {
+                    WIFISSID targetSSID = new WIFISSID();
+  
+                    targetSSID.wlanInterface = wlanIface;
+                    targetSSID.wlanSignalQuality = (int)network.wlanSignalQuality;
+                    targetSSID.SSID = GetStringForSSID(network.dot11Ssid);
+                    targetSSID.dot11DefaultAuthAlgorithm = network.dot11DefaultAuthAlgorithm.ToString();
+                    targetSSID.dot11DefaultCipherAlgorithm = network.dot11DefaultCipherAlgorithm.ToString();
+                    ssids.Add(targetSSID);
+                }
+            }
+        } // EnumSSID
+  
+        public static string StringToHex(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] byStr = System.Text.Encoding.Default.GetBytes(str); //默认是System.Text.Encoding.Default.GetBytes(str)
+            for (int i = 0; i < byStr.Length; i++)
+            {
+                sb.Append(Convert.ToString(byStr[i], 16));
+            }
+  
+            return (sb.ToString().ToUpper());
+        }
+
+
+    }
+}
